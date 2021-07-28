6c9d28ee8211fd2b696d4a1d682613e9258cb188
NickDienemann <nick@eckel-dienemann.de>
2021-07-28 21:27:10 +0200

    added data exploration in notebook

--- translater.py
+++ translater.py
@@ -6,12 +6,6 @@ this file will be used to transform data sets into english
 from googletrans import Translator
 import pandas as pd
 
-#dataformat
-"""
-pd.DataFrame
-columns: index, text, label
-"""
-
 
 def translate_df(df,col_name,src_lan,dest_lan, full_dest_path=None):
     """
@@ -23,11 +17,10 @@ def translate_df(df,col_name,src_lan,dest_lan, full_dest_path=None):
     #initialize the translator
     translator= Translator()
 
-    #define an internal function that takes a pd.Series and translates each element
-    #def translator_func():
-
+    #define a translation function that returns the translated text
     translator_func = lambda sentence,dest_lan,src_lan: translator.translate(sentence,dest_lan,src_lan).text
 
+    #apply the translation function to the dataframe
     df[col_name] = df[col_name].apply(translator_func,args=[dest_lan,src_lan])
 
     #if a path is given store the df thus created
@@ -35,21 +28,6 @@ def translate_df(df,col_name,src_lan,dest_lan, full_dest_path=None):
         df.to_csv(full_dest_path+".csv")
 
     return df
-
-#initialize the translator
-"""
-translator=Translator()
-
-text= "特写 ： 特朗普 在 联合国 的 90 分钟 记者会"  #input("enter your text: ")
-source_lan= "zh-cn"
-translated_to ="en"
-
-translated_text = translator.translate(text,src=source_lan,dest=translated_to)
-
-print(text)
-print(translated_text.text)
-"""
-
 
 if __name__=="__main__":
     #original_df=pd.DataFrame()
